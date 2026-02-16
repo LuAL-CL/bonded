1772c77524fa65e4f6f68abf779286f5d0752a14
LuAL-CL <luis.caceresl@gmail.com>
2026-02-16 15:44:36 -0300

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["@bonded/shared"],
   experimental: {
     serverActions: {
-      bodySizeLimit: "10mb"
-    }
-  }
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
